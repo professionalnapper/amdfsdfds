bce1d0cf36dcbdc77bdd6fd7256fef36fc887f11
professionalnapper <quiamcocristal2829@gmail.com>
2024-05-01 18:40:58 +0800

    asdsa

---
+++ Dental-dashboard/src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Aos from 'aos';
+import Dashboard from './screens/Dashboard';
+import Toast from './components/Notifications/Toast';
+import Payments from './screens/Payments/Payments';
+import Appointments from './screens/Appointments';
+import Patients from './screens/Patients/Patients';
+import Services from './screens/Services';
+import Invoices from './screens/Invoices/Invoices';
+import Settings from './screens/Settings';
+import CreateInvoice from './screens/Invoices/CreateInvoice';
+import EditInvoice from './screens/Invoices/EditInvoice';
+import PreviewInvoice from './screens/Invoices/PreviewInvoice';
+import EditPayment from './screens/Payments/EditPayment';
+import PreviewPayment from './screens/Payments/PreviewPayment';
+import PatientProfile from './screens/Patients/PatientProfile';
+import CreatePatient from './screens/Patients/CreatePatient';
+import Doctors from './screens/Doctors/Doctors';
+import DoctorProfile from './screens/Doctors/DoctorProfile';
+import Receptions from './screens/Receptions';
+import NewMedicalRecode from './screens/Patients/NewMedicalRecode';
+import NotFound from './screens/NotFound';
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+
+function App() {
+  Aos.init();
+
+  return (
+    <>
+      {/* Toaster */}
+      <Toast />
+      {/* Routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {/* invoce */}
+          <Route path="/invoices" element={<Invoices />} />
+          <Route path="/invoices/create" element={<CreateInvoice />} />
+          <Route path="/invoices/edit/:id" element={<EditInvoice />} />
+          <Route path="/invoices/preview/:id" element={<PreviewInvoice />} />
+          {/* payments */}
+          <Route path="/payments" element={<Payments />} />
+          <Route path="/payments/edit/:id" element={<EditPayment />} />
+          <Route path="/payments/preview/:id" element={<PreviewPayment />} />
+          {/* patient */}
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/patients/preview/:id" element={<PatientProfile />} />
+          <Route path="/patients/create" element={<CreatePatient />} />
+          <Route path="/patients/visiting/:id" element={<NewMedicalRecode />} />
+          {/* doctors */}
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/doctors/preview/:id" element={<DoctorProfile />} />
+          {/* reception */}
+          <Route path="/receptions" element={<Receptions />} />
+          {/* others */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
